fd0466101f07974b30f673301a4ae163ac711dd0
Karunya Sri <karunyak597@gmail.com>
2026-04-02 13:25:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,11 +25,9 @@ with col_center:
     col1, col2 = st.columns([1, 9])
 
     with col1:
-        st.markdown("""
-        <div style="display:flex; justify-content:flex-end; padding-top:10px;">
-            <img src="icon.PNG" width="60">
-        </div>
-        """, unsafe_allow_html=True)
+        st.markdown("<div style='padding-top:10px; text-align:right;'>", unsafe_allow_html=True)
+        st.image("icon.PNG", width=60)
+        st.markdown("</div>", unsafe_allow_html=True)
 
     with col2:
         st.markdown("""
